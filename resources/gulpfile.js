0300bc698f19af844d27e9781e7a0342cfc7b4b2
wxwdesign <wxwdesign@gmail.com>
2014-12-02 17:10:02 +0800

    update rename

---
+++ resources/gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require("gulp");
+var less = require("gulp-less");
+
+gulp.task("less", function(){
+    return gulp.src(["less/*.less"])
+            .pipe(less())
+            .pipe(gulp.dest("css/"));
+});
+
+gulp.task("watch", function(){
+    gulp.watch(["less/*.less"], function(){
+        gulp.run("less");
+    });
+});
+
+gulp.task("default", ["less"]);
